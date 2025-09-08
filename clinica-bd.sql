073481d25ab0f9b3e68bc28ca295f5ecc45eb537
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2025-09-08 20:33:34 -0300

    Creación de la BD y sus tablas

---
+++ clinica-bd.sql
@@ -0,0 +1,61 @@
+# Creación de la BD
+ CREATE DATABASE IF NOT EXISTS clinica;
+ 
+ # Creación de las tablas
+ # 1. Médico
+ CREATE TABLE IF NOT EXISTS medico(
+ );
+ 
+ 
+ # 2. Paciente
+ CREATE TABLE IF NOT EXISTS paciente(
+ 
+ );
+ 
+ 
+  # 3. Turno
+ CREATE TABLE IF NOT EXISTS turno(
+ 
+ );
+ 
+ # 4. Consultorio
+ CREATE TABLE IF NOT EXISTS consultorio(
+ 
+ );
+ 
+ # 5. Diagnóstico
+ CREATE TABLE IF NOT EXISTS diagnostico(
+ 
+ );
+
+
+# 5. Diagnóstico
+ CREATE TABLE IF NOT EXISTS diagnostico(
+ 
+ );
+
+# 6. Historial_paciente
+ CREATE TABLE IF NOT EXISTS historial_paciente(
+ 
+ );
+
+# 7. Factura
+ CREATE TABLE IF NOT EXISTS factura(
+ 
+ );
+ 
+ # 8.  Estado_turno
+ CREATE TABLE IF NOT EXISTS  estado_turno(
+ 
+ );
+ 
+ # 9. Motivo_cancelación
+ CREATE TABLE IF NOT EXISTS motivo_cancelacion(
+ 
+ );
+ # 10. Resultado_estudio 
+ CREATE TABLE IF NOT EXISTS resultado_estudio (
+ 
+ );
+ 
+ 
