d58331a1ca884888ac7011255d2b9e8e286e37be
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2025-09-23 17:49:50 -0300

    Nuevos cambios guardados

--- clinica-bd.sql
+++ clinica-bd.sql
@@ -36,7 +36,7 @@ CREATE TABLE paciente (
     fecha_nacimiento DATE NOT NULL,
     telefono VARCHAR(20) NOT NULL 
     );
-    
+
 -- 3. Estado del turno
 CREATE TABLE estado_turno (
     id_estado_turno INT AUTO_INCREMENT PRIMARY KEY,
@@ -51,31 +51,23 @@ CREATE TABLE consultorio (
     piso INT NOT NULL
 );
 
--- 5. Motivo de cancelación
-CREATE TABLE motivo_cancelacion (
-    id_motivo_cancelacion INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL UNIQUE,
-    descripcion VARCHAR(100)
-);
-
--- 6. Turno
+-- 5. Turno
 CREATE TABLE turno (
     id_turno INT AUTO_INCREMENT PRIMARY KEY,
     id_paciente INT NOT NULL,
     id_medico INT NOT NULL,
     id_consultorio INT NOT NULL,
     id_estado_turno INT NOT NULL,
-    id_motivo_cancelacion INT NULL,
     fecha DATE NOT NULL,
     hora TIME NOT NULL,
     CONSTRAINT fk_turno_paciente     FOREIGN KEY (id_paciente)      REFERENCES paciente(id_paciente),
     CONSTRAINT fk_turno_medico       FOREIGN KEY (id_medico)        REFERENCES medico(id_medico),
     CONSTRAINT fk_turno_consultorio  FOREIGN KEY (id_consultorio)   REFERENCES consultorio(id_consultorio),
-    CONSTRAINT fk_turno_estado       FOREIGN KEY (id_estado_turno)  REFERENCES estado_turno(id_estado_turno),
-    CONSTRAINT fk_turno_motivo       FOREIGN KEY (id_motivo_cancelacion) REFERENCES motivo_cancelacion(id_motivo_cancelacion)
+    CONSTRAINT fk_turno_estado       FOREIGN KEY (id_estado_turno)  REFERENCES estado_turno(id_estado_turno)
 );
 
--- 7. Diagnóstico
+
+-- 6. Diagnóstico
 CREATE TABLE diagnostico (
     id_diagnostico INT AUTO_INCREMENT PRIMARY KEY,
     id_turno INT NOT NULL,
@@ -84,16 +76,15 @@ CREATE TABLE diagnostico (
     CONSTRAINT fk_diagnostico_turno FOREIGN KEY (id_turno) REFERENCES turno(id_turno)
 );
 
--- 8. Factura
+-- 7. Factura
 CREATE TABLE factura (
     id_factura INT AUTO_INCREMENT PRIMARY KEY,
     id_paciente INT NOT NULL,
     fecha DATE NOT NULL,
-    monto DECIMAL(10,2) NOT NULL, 
     CONSTRAINT fk_factura_paciente FOREIGN KEY (id_paciente) REFERENCES paciente(id_paciente)
 );
 
--- 9. Resultado de estudio
+-- 8. Resultado de estudio
 CREATE TABLE resultado_estudio (
     id_resultado_estudio INT AUTO_INCREMENT PRIMARY KEY,
     id_paciente INT NOT NULL,
@@ -103,13 +94,13 @@ CREATE TABLE resultado_estudio (
     CONSTRAINT fk_resultado_paciente FOREIGN KEY (id_paciente) REFERENCES paciente (id_paciente)
 );
 
--- 10. Tipo de evento
+-- 9. Tipo de evento
 CREATE TABLE tipo_evento (
     id_tipo_evento INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL UNIQUE
 );
 
--- 11. Historial del paciente
+-- 10. Historial del paciente
 CREATE TABLE historial_paciente (
     id_historial_paciente INT AUTO_INCREMENT PRIMARY KEY,
     id_paciente INT NOT NULL,
@@ -119,7 +110,8 @@ CREATE TABLE historial_paciente (
     CONSTRAINT fk_historial_paciente FOREIGN KEY (id_paciente) REFERENCES paciente(id_paciente),
     CONSTRAINT fk_historial_evento   FOREIGN KEY (id_tipo_evento) REFERENCES tipo_evento(id_tipo_evento)
 );
--- 12. especialidad 
+
+-- 11. especialidad 
 CREATE TABLE especialidad(
 id_especialidad INT AUTO_INCREMENT PRIMARY KEY,
 nombre VARCHAR(20) NOT NULL,
@@ -127,7 +119,7 @@ descripcion VARCHAR(150) NOT NULL
 );
 
 
--- 13. telefono_paciente
+-- 12. telefono_paciente
 CREATE TABLE telefono_paciente(
 id_telefono INT AUTO_INCREMENT PRIMARY KEY,
 id_paciente INT NOT NULL,
@@ -136,10 +128,103 @@ tipo VARCHAR(20),
 CONSTRAINT fk_paciente_medico FOREIGN KEY (id_paciente) REFERENCES paciente(id_paciente)
 );
 
--- 14. Obra Social
+-- 13. Obra Social
 CREATE TABLE obra_social(
  id_obra_social INT AUTO_INCREMENT PRIMARY KEY,
  nombre VARCHAR(50) NOT NULL,
  plan VARCHAR(50) NOT NULL
 );
 
+-- 14. telefono_medico
+CREATE TABLE telefono_medico(
+    id_telefono_medico int auto_increment primary key,
+    id_medico int not null,
+    telefono varchar(20) not null,
+    tipo varchar(20) NOT NULL,
+    CONSTRAINT FK_medico_telefono foreign key (id_medico) REFERENCES medico(id_medico)
+);
+
+-- 15. Creamos una tabla intermedia para una relación de muchos a muchos entre Médico y Especialidad
+CREATE TABLE medico_especialidad (
+    id_medico INT,
+    id_especialidad INT,
+    PRIMARY KEY (id_medico, id_especialidad),
+    FOREIGN KEY (id_medico) REFERENCES medico(id_medico),
+    FOREIGN KEY (id_especialidad) REFERENCES especialidad(id_especialidad)
+);
+
+-- Agregación de campo 'Asistencia' tabla turno
+ALTER TABLE turno ADD COLUMN asistencia boolean;
+
+-- Vinculamos Obra Social con Paciente
+ALTER TABLE paciente ADD COLUMN id_obra_social INT;
+
+-- Crear la clave foranea
+ALTER TABLE paciente
+ADD CONSTRAINT fk_paciente_obra
+FOREIGN KEY (id_obra_social)
+REFERENCES obra_social(id_obra_social);
+
+-- Agregación de campo 'monto_total' tabla Factura
+ALTER TABLE factura ADD COLUMN monto_total DECIMAL(10,2) NOT NULL;
+
+-- Agregación de campo 'monto_cubierto_obra_social' tabla Factura
+ALTER TABLE factura ADD COLUMN  monto_cubierto_obra_social DECIMAL (10,2) NOT NULL; 
+
+-- Agregación de campo 'monto_afiliado' tabla Factura
+ALTER TABLE factura ADD COLUMN monto_afiliado DECIMAL(10,2) NOT NULL;
+
+-- Vinculamos Médico con Especialidad
+ALTER TABLE medico 
+ADD COLUMN id_especialidad INT;
+
+-- Crear la clave foránea
+ALTER TABLE medico 
+ADD CONSTRAINT fk_especialidad_medico
+FOREIGN KEY (id_especialidad)
+REFERENCES especialidad (id_especialidad);
+
+-- Vinculamos Médico con Consultorio
+ALTER TABLE medico ADD COLUMN id_consultorio INT;
+
+-- Crear la clave foranea
+ALTER TABLE medico
+ADD CONSTRAINT fk_consultorio_medico
+FOREIGN KEY (id_consultorio)
+REFERENCES consultorio (id_consultorio);
+
+-- Vinculamos Diagnostico con Médico
+ALTER TABLE diagnostico ADD COLUMN id_medico INT;
+
+-- Crear la clave foranea
+ALTER TABLE diagnostico
+ADD CONSTRAINT fk_medico_diagnostico
+FOREIGN KEY (id_medico)
+REFERENCES medico (id_medico);
+
+
+-- Eliminación de motivo_cancelacion
+ALTER TABLE turno 
+DROP FOREIGN KEY id_motivo_cancelacion;
+
+ALTER TABLE turno 
+DROP FOREIGN KEY fk_turno_motivo;
+
+-- Eliminación tabla Motivo Cancelación
+DROP TABLE motivo_cancelacion;
+
+
+ALTER TABLE diagnostico 
+DROP FOREIGN KEY fk_diagnostico_turno;
+
+
+-- Eliminación de diagnostico
+ALTER TABLE diagnostico
+DROP COLUMN id_turno;
+
+-- Eliminación de medico
+ALTER TABLE medico 
+DROP FOREIGN KEY fk_diagnostico_medico;
+
+ALTER TABLE medico
+DROP COLUMN id_diagnostico;
